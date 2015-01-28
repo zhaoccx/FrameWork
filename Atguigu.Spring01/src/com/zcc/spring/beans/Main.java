785b87c4ec1f3113ae2aac8e14bbe1657e1dfb5a
zhaoccx <zhaoccx@hotmail.com>
2015-01-28 13:18:18 +0800

    Init

---
+++ Atguigu.Spring01/src/com/zcc/spring/beans/Main.java
@@ -0,0 +1,10 @@
+package com.zcc.spring.beans;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HelloWorld helloWorld = new HelloWorld();
+		helloWorld.setName("words");
+		helloWorld.hello();
+	}
+}
