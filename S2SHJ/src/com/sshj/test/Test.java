7799cbd0b59f1e40fa6c9f4b55688bb2cdd2b98b
趙常春 <zhaoccx@hotmail.com>
2016-07-21 09:14:04 +0800

    test

--- S2SHJ/src/com/sshj/test/Test.java
+++ S2SHJ/src/com/sshj/test/Test.java
@@ -3,10 +3,18 @@
  */
 package com.sshj.test;
 
+import java.util.Calendar;
+
 /**
  * @author zhaocc
  *
  */
 public class Test {
+
+	public static void main(String[] args) {
+		Calendar instance = Calendar.getInstance();
+		System.out.println(instance.getMinimalDaysInFirstWeek());
+		System.out.println(instance.get(Calendar.WEEK_OF_YEAR));
+	}
 
 }
